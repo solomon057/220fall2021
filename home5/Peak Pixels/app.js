e4cd88383e3b8cb5e6f4436285475700aabc2264
solomon057 <89545888+solomon057@users.noreply.github.com>
2021-12-08 20:19:24 -0500

    Home 5

--- home5/Peak Pixels/app.js
+++ home5/Peak Pixels/app.js
@@ -1,15 +1,16 @@
-
-var myDiv= document.createElement("DIV");
-document.body.appendChild(myDiv);
-
-myDiv.style.width="100px"
-myDiv.style.height='100px'
-myDiv.style.backgroundColor='green'
-
-myDiv.onclick = function () {
-    myDiv.style.width *= 1.1 + "px";
-    myDiv.style.height *= 1.1 + "px";
-
+// function fun() created here
+ function fun() {
+    //accessing the div using id square
+    var x = document.getElementById('square');
+    //getting the width from the div and increasing 10% , adding px to it. 
+    var div_width = x.offsetWidth + (0.1 * x.offsetWidth)+"px";
+    //getting the height from the div and increasing 10% , adding px to it.
+    var div_height = x.offsetHeight + (0.1 * x.offsetHeight)+"px";
+    //Setting the width of div to new div_width
+    x.style.width = div_width;
+    //Setting the height of the div to new div_height
+    x.style.height = div_height;
 }
+
 
     
